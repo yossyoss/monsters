bdfdb6fc7e3e63abcb08eb00f7122e3ccb978f83
yossi <yossi@hiredscore.com>
2022-05-24 21:12:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    monsters : [
+      {
+        name: 'yoss',
+        id: 123213
+      },
+      {
+        name: 'ell',
+        id: 444
+    },
+      {
+        name: 'bell',
+        id: 555
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+         <div>
+         {this.state.monsters.map(mon => {
+         return <h1 key={mon.id}>{mon.name}</h1>
+         })}
+           </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
